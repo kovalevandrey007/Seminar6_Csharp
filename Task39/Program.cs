278591897c0b8ab750bfd6d6c9abf94edb48d307
Андрей Ковалев <kovalevandrey007@gmail.com>
2022-12-12 00:42:39 +0200

    1

---
+++ Task39/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]*/
+
+//метод, который будет выдавать заполненный массив случайными числами
+int[] InitArray(int dimension)
+{
+int[] array = new int[dimension];
+Random rnd = new Random();
+
+for (int i = 0; i < dimension; i++)
+{
+array[i] = rnd.Next(1,9);
+}
+
+return array;
+}
+
+//метод, который будет печатать массив
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+
+//получение числа с консоли, для того, чтобы задать размерность массива
+int GetNumberFromConsole(string message)
+{
+bool isCorrect = false;
+int result = 0;
+
+while(!isCorrect)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result))
+{
+isCorrect = true;
+}
+else
+{
+Console.WriteLine("Ввели не число. Введите число");
+}
+}
+return result;
+}
