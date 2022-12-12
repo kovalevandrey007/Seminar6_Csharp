9b55a8edc8be385634d8866d169a6d9f9c5502b4
Андрей Ковалев <kovalevandrey007@gmail.com>
2022-12-12 23:51:55 +0200

    1

---
+++ Task40/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 40: Напишите программу,
+ которая принимает на вход три числа 
+  проверяет, может ли существовать треугольник с сторонами такой длины.*/
+
+int  GetNumberFromConsole(string message)
+{
+bool isCorrect = false;
+int result = 0;
+
+while(!isCorrect)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result))
+{
+isCorrect = true;
+}
+else
+{
+Console.WriteLine("Ввели не число. Введите число");
+}
+}
+return result;
+}
+int a = GetNumberFromConsole("введите первую сторону");
+int b = GetNumberFromConsole("введите вторую сторону");
+int с = GetNumberFromConsole("введите третью сторону");
+
+if ((a+b)>с && (b+с)>a && (a+с)>b)
+{
+Console.WriteLine("триугольник сушествует: ");
+}
+else
+Console.WriteLine("триугольник не сушествует: ");
--- Task42/Program.cs
+++ Task42/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 42: Напишите программу, которая 
+будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+int GetNumber(string text)
+{
+Console.WriteLine(text);
+bool isCorrect = false;
+int result = 0;
+while (!isCorrect)
+{
+if (int.TryParse(Console.ReadLine(), out result))
+{
+isCorrect = true;
+}
+else Console.WriteLine("Ввели не число");
+}
+return result;
+}
+
+string DecToBin(int decNumber)
+{
+string result = string.Empty;
+while (decNumber != 0)
+{
+result = result + decNumber % 2;
+decNumber = decNumber / 2;
+}
+return result;
+}
+int decNumber = GetNumber("Введите число");
+string result = DecToBin(decNumber);
+Console.WriteLine(new string(result.Reverse().ToArray()));
